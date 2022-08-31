0256efa7f5f22d6bd924a511acce2cdaf9a005f7
mawburn <mawburn7@gmail.com>
2022-08-31 00:43:31 -0400

    update font

--- src/components/SponsoredBy/index.tsx
+++ src/components/SponsoredBy/index.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
+import cn from 'clsx'
 
 import { baseConfig } from '../../baseConfig'
+import styles from './styles.module.css'
 
 export interface SponsoredByProps {
   className?: string
 }
 
 export const SponsoredBy = ({ className = '' }: SponsoredByProps) => (
-  <div className="sm:mx-auto mx-2 sm:mb-4 my-1 w-fit text-center">
-    <div
-      className={`flex rounded-md self-center p-2 mb-4 bg-gray-100	border border-gray-300	shadow-md ${className}`}
-    >
+  <div className={styles.wrapper}>
+    <div className={cn(styles.innerWrapper, className)}>
       <a
-        className="flex flex-wrap justify-center gap-4 items-center h-full no-underline"
+        className={styles.link}
         href="https://tabletop.land"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <div className="flex flex-col items-center justify-center bg-ttl p-2 rounded-lg">
+        <div className={styles.imgContainer}>
           <img
             src={`${baseConfig.imgBucket}/tabletopland.png`}
             width={75}
@@ -25,15 +25,11 @@ export const SponsoredBy = ({ className = '' }: SponsoredByProps) => (
             alt="tabletop.land"
             loading="eager"
           />
-          <h2 className="font-ttl tracking-widest text-ttlFont sm:text-normal text-sm font-bold font-normal">
-            Tabletop.Land
-          </h2>
+          <h2 className={styles.title}>Tabletop.Land</h2>
         </div>
-        <div className="flex flex-col gap-2 items-center justify-center">
+        <div className={styles.infoWrapper}>
           <div className="text-xs">Built &amp; Sponsored by Tabletop.Land</div>
-          <div className="font-ttl tracking-widest sm:text-sm text-xs font-normal">
-            Buy 3D Printed minis &amp; handcrafted Terrain!
-          </div>
+          <div className={styles.infoDetails}>Buy 3D Printed minis &amp; handcrafted Terrain!</div>
         </div>
       </a>
     </div>
